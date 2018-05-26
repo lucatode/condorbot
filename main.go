0512ebc4f225cab0f4c11d1bab7f808dc56d268c
DESKTOP-GFAGTVL\Luca <l.todeschini88@gmail.com>
2018-05-27 01:13:21 +0200

    porting bot setup

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"condor/initializer"
+	"os"
+	"log"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func Init(args []string) initializer.Initializer {
+	if len(args) > 1 {
+		return initializer.NewInitializer(initializer.NewJsonReader(args[1]))
+	}
+	return initializer.NewInitializer(initializer.NewJsonReader("initializer_test.json"))
+}
 
 func main(){
-	fmt.Printf("Hello World")
+	//INIT
+	initializer := Init(os.Args)
+
+	// SETUP BOT
+	_, err := tgbotapi.NewBotAPI(initializer.GetApiToken())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
