8c1797c47cf6c48b6a1df0674ca5890f63a60182
ltodeschini <Ramones$1>
2018-05-29 23:38:34 +0200

    adding glide

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/telegram-bot-api.v4"
 )
 
 func Init(args []string) initializer.Initializer {
 	if len(args) > 1 {
 		return initializer.NewInitializer(initializer.NewJsonReader(args[1]))
 	}
-	return initializer.NewInitializer(initializer.NewJsonReader("initializer_test.json"))
+	return initializer.NewInitializer(initializer.NewJsonReader("initializer.json"))
 }
 
 func NotifyChannel(w http.ResponseWriter, r *http.Request) {
@@ -24,19 +25,19 @@ func NotifyChannel(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	//INIT
-	// initializer := Init(os.Args)
+	initializer := Init(os.Args)
 
 	// SETUP BOT
-	// bot, err := tgbotapi.NewBotAPI(initializer.GetApiToken())
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	bot, err := tgbotapi.NewBotAPI(initializer.GetApiToken())
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// BOT CONFIG
-	// _, err = bot.SetWebhook(tgbotapi.NewWebhook(initializer.GetServerUrl() + "" + bot.Token))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(initializer.GetServerUrl() + "" + bot.Token))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// SETUP INPUT ROUTES
 	port := os.Getenv("PORT")
@@ -44,5 +45,19 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/notify/{channel}", NotifyChannel).Methods("GET")
 	log.Fatal(http.ListenAndServe(":"+port, router))
+
+	// FETCH MESSAGES
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		// //  REGISTERING TO A CHANNEL
+
+		// //  REPLY TO A MESSAGE
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		bot.Send(msg)
+	}
 
 }
