2e5c7e746b68088c015d89d2e093b0e4483d974f
ltodeschini <Ramones$1>
2018-05-29 22:51:17 +0200

    creating notify service

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"condor/initializer"
-	"os"
+	"condorbot/initializer"
 	"log"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func Init(args []string) initializer.Initializer {
@@ -14,23 +16,33 @@ func Init(args []string) initializer.Initializer {
 	return initializer.NewInitializer(initializer.NewJsonReader("initializer_test.json"))
 }
 
-func main(){
+func NotifyChannel(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	message := "Notifing channel: " + params["channel"]
+	w.Write([]byte(message))
+}
+
+func main() {
 	//INIT
-	initializer := Init(os.Args)
+	// initializer := Init(os.Args)
 
 	// SETUP BOT
-	bot, err := tgbotapi.NewBotAPI(initializer.GetApiToken())
-	if err != nil {
-		log.Fatal(err)
-	}
+	// bot, err := tgbotapi.NewBotAPI(initializer.GetApiToken())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// BOT CONFIG
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(initializer.GetServerUrl() + "" + bot.Token))
-	if err != nil {
-	log.Fatal(err)
-	}
+	// _, err = bot.SetWebhook(tgbotapi.NewWebhook(initializer.GetServerUrl() + "" + bot.Token))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-
-	//port := os.Getenv("PORT")
+	// SETUP INPUT ROUTES
+	port := os.Getenv("PORT")
+	port = "8080"
+	router := mux.NewRouter()
+	router.HandleFunc("/notify/{channel}", NotifyChannel).Methods("GET")
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
