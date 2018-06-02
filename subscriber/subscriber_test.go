17d1df35922f35423cd76f44790d165d1a9208c7
ltodeschini <Ramones$1>
2018-06-02 11:57:55 +0200

    fix

--- subscriber/subscriber_test.go
+++ subscriber/subscriber_test.go
@@ -1,18 +1,1 @@
 package subscriber
-
-import (
-	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestGetAPITocken(t *testing.T) {
-	message := "#subscribe channel xxxx"
-	split := strings.Split(message, " ")
-	response := AddSubscription("https://condorbot-c36af.firebaseio.com/subscriptions", split, "chatId 1")
-	AddSubscription("https://condorbot-c36af.firebaseio.com/subscriptions", split, "chatId 2")
-	AddSubscription("https://condorbot-c36af.firebaseio.com/subscriptions", split, "chatId 3")
-
-	assert.Equal(t, "Subscribed this chat to channel xxxx", response, "Getting specific match bool")
-}
