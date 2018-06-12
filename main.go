bcf64812cc5b1e31d90a2975f383c952f3f20978
ltodeschini <Ramones$1>
2018-06-12 18:36:46 +0200

    removing unused logs

--- main.go
+++ main.go
@@ -100,7 +100,6 @@ func main() {
 	_, err = bot.SetWebhook(tgbotapi.NewWebhook(init.GetServerUrl() + bot.Token))
 	if err != nil {
 		log.Fatal(err)
-
 	}
 
 	// SETUP INPUT ROUTES
@@ -118,11 +117,6 @@ func main() {
 		}
 
 		ok, text := p.ParseMessage(BuildMessage(update.Message))
-		//if ok {
-		//	logger.Log("MAIN", "Response found"+text)
-		//}else{
-		//	logger.Log("MAIN", "Response not found, message was "+update.Message.Text)
-		//}
 
 		if ok {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
